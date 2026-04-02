6b1152a8d59f45cca3bddc8efe237839a3209696
victormellos <victormellos@outlook.com>
2026-04-01 21:41:06 -0300

    FEAT: Add funções de formatarMoeda, calcularDesconto e variáveis de produto

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,3 +1,20 @@
+const nomeProduto = "cadeira legal";
+const produtoAtivo = true; 
+let quantidadeEstoque;
+
 function saudarCliente(Nome) {
     return `Olá, ${Nome}! Bem-vindo à nossa loja.`;
+}
+
+function formatarMoedaBRL(valor){
+    return valor.toFixed(2)
+}
+
+function calcularDesconto(precoOriginal, isFuncionario){
+    if (isFuncionario){
+        const desconto = precoOriginal * 0.30;
+        return precoOriginal - desconto;
+    }else{
+        return precoOriginal
+    }
 }
